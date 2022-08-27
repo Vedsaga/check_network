ecf7e23e5c73b4fbab7dc63fd13d3b1a192a0bbf
vedsaga <harshit7kr@gmail.com>
2022-08-27 18:48:23 +0530

    removed not used file

--- lib/check_network.dart
+++ lib/check_network.dart
@@ -1,14 +1,13 @@
 library check_network;
 
 export 'constants/address_check_status.dart';
-export 'constants/call_back.dart';
-export 'constants/data_connection_status.dart';
+export 'constants/connection_status.dart';
 export 'constants/internet_status.dart';
 export 'models/address_check_option.dart';
 export 'models/address_check_result.dart';
 export 'models/default.dart';
+export 'src/check_internet.dart';
 export 'src/current_internet_status.dart';
 export 'src/current_internet_status.dart';
-export 'src/internet_data_checker.dart';
 export 'src/internet_status_provider.dart';
 export 'widgets/internet_status_header.dart';
--- lib/constants/call_back.dart
+++
@@ -1,9 +0,0 @@
-/// This is type of callback that is used to send data back to the native side.
-///
-enum Callbacks {
-  /// this is used to uploading the data.
-  upload,
-
-  /// this is used to downloading the data.
-  download,
-}
